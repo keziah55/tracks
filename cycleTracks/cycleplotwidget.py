d213650d3001fd94da3255893ff0971f5f05069e
keziah55 <19816994+keziah55@users.noreply.github.com>
2020-11-28 15:30:41 +0000

    odometer fill

--- cycleTracks/cycleplotwidget.py
+++ cycleTracks/cycleplotwidget.py
@@ -5,40 +5,17 @@ Subclass of pyqtgraph.PlotWidget.
 """
 
 from datetime import datetime
-from pyqtgraph import PlotWidget, DateAxisItem, mkPen, mkBrush
+from pyqtgraph import (PlotWidget, DateAxisItem, PlotCurveItem, ViewBox, mkPen, 
+                       mkBrush)
 import numpy as np
+from PyQt5.QtCore import pyqtSlot as Slot
 
-class CyclePlotWidget(PlotWidget):
+
+class CycleData:
+    
     def __init__(self, df):
-        super().__init__(axisItems={'bottom': DateAxisItem()})
-        
         self.df = df
         
-        self.colour = "#024aeb"
-        style = self._makeStyle(self.colour)
-        
-        date = [datetime.strptime(d, "%Y-%m-%d").timestamp() for d in self.df['Date']]
-        distance = np.array(self.df['Distance (km)'])
-        time = [self._timeToSecs(t) for t in self.df['Time']]
-        time = np.array([self._convertSecs(s) for s in time])
-        speed = distance/time
-        
-        self.plotItem.plot(date, speed, **style)
-        self.plotItem.showGrid(x=True, y=True)
-        self.plotItem.setLabels(left='Avg. speed, (km/h)', bottom='Date')
-        
-        # bug workaround - we don't need units/SI prefix on dates
-        # this has been fixed in the pyqtgraph source, so won't be necessary
-        # once this makes its way into the deb packages
-        self.plotItem.getAxis('bottom').enableAutoSIPrefix(False)
-        
-    @staticmethod
-    def _makeStyle(colour):
-        pen = mkPen(colour)
-        brush = mkBrush(colour)
-        d = {'pen':None, 'symbol':'x', 'symbolPen':pen, 'symbolBrush':brush}
-        return d
-    
     @staticmethod
     def _timeToSecs(t):
         msg = ''
@@ -72,3 +49,115 @@ class CyclePlotWidget(PlotWidget):
         h = m / 60
         if mode == 'hour':
             return h
+        
+    @property
+    def distance(self):
+        return np.array(self.df['Distance (km)'])
+    
+    @property
+    def time(self):
+        time = [self._timeToSecs(t) for t in self.df['Time']]
+        time = np.array([self._convertSecs(s) for s in time])
+        return time
+    
+    @property
+    def dateTimestamps(self):
+        return [dt.timestamp() for dt in self.datetimes]
+
+    @property
+    def datetimes(self):
+        return [datetime.strptime(d, "%Y-%m-%d") for d in self.df['Date']]
+    
+
+class CyclePlotWidget(PlotWidget):
+    def __init__(self, df):
+        super().__init__(axisItems={'bottom': DateAxisItem()})
+        
+        self.data = CycleData(df)
+        
+        self.style = {'speed':{'colour':"#024aeb"},
+                      'odometer':{'colour':"#178a00"}}
+        
+        self._initRightAxis('Total distance', self.style['odometer']['colour'])
+        
+        style = self._makeScatterStyle(self.style['speed']['colour'])
+        speed = self.data.distance/self.data.time
+        self.plotItem.plot(self.data.dateTimestamps, speed, **style)
+        
+        
+        # self.plotItem.showGrid(x=True, y=True)
+        self.plotItem.setLabels(left='Avg. speed, (km/h)', bottom='Date')
+        
+        style = self._makeFillStyle(self.style['odometer']['colour'])
+        odo = self._getMonthlyOdometer()
+        curve = PlotCurveItem(self.data.dateTimestamps, odo, **style)
+        self.vb2.addItem(curve)
+        
+        # style = self._makeStyle("#cf0202")
+        # self.plotItem.plot(date, self.df['Distance (km)'], **style)
+        # # style = self._makeStyle("#19b536")
+        # # self.plotItem.plot(date, self.df['Time'], **style)
+        # style = self._makeStyle("#ff9100")
+        # self.plotItem.plot(date, self.df['Calories'], **style)
+        
+        self.plotItem.getAxis('left').setGrid(255)
+        self.plotItem.getAxis('bottom').setGrid(255)
+        
+        # bug workaround - we don't need units/SI prefix on dates
+        # this has been fixed in the pyqtgraph source, so won't be necessary
+        # once this makes its way into the deb packages
+        self.plotItem.getAxis('bottom').enableAutoSIPrefix(False)
+        
+        self.updateViews()
+        self.plotItem.vb.sigResized.connect(self.updateViews)
+        
+        
+    def _initRightAxis(self, label, colour):
+        
+        self.vb2 = ViewBox()
+        self.plotItem.showAxis('right')
+        self.plotItem.scene().addItem(self.vb2)
+        self.plotItem.getAxis('right').linkToView(self.vb2)
+        self.vb2.setXLink(self.plotItem)
+        self.plotItem.getAxis('right').setLabel(label, color=colour)
+        
+        
+    @Slot()
+    def updateViews(self):
+        ## Copied from PyQtGraph MultiplePlotAxes.py example ##
+        # view has resized; update auxiliary views to match
+        self.vb2.setGeometry(self.plotItem.vb.sceneBoundingRect())
+        # need to re-update linked axes since this was called
+        # incorrectly while views had different shapes.
+        # (probably this should be handled in ViewBox.resizeEvent)
+        self.vb2.linkedViewChanged(self.plotItem.vb, self.vb2.XAxis)
+    
+    @staticmethod
+    def _makeScatterStyle(colour):
+        pen = mkPen(colour)
+        brush = mkBrush(colour)
+        d = {'pen':None, 'symbol':'x', 'symbolPen':pen, 'symbolBrush':brush}
+        return d
+
+    @staticmethod
+    def _makeFillStyle(colour):
+        pen = mkPen(colour)
+        brush = mkBrush(colour)
+        d = {'pen':pen, 'brush':brush, 'fillLevel':0}
+        return d
+
+
+    def _getMonthlyOdometer(self):
+        
+        months = [dt.month for dt in self.data.datetimes]
+        
+        odo = np.zeros(len(self.data.distance))
+        prev = 0
+        for i in range(len(self.data.distance)):
+            if i == 0 or months[i] != months[i-1]:
+                prev = 0
+            else:
+                prev = odo[i-1]
+            odo[i] = prev + self.data.distance[i]
+            
+        return odo
