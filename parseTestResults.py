deddba30d310dd2a4ef0226e735886c823970a6d
keziah55 <19816994+keziah55@users.noreply.github.com>
2022-02-16 22:40:45 +0000

    test summary script converts (most) xml data to html report

--- parseTestResults.py
+++ parseTestResults.py
@@ -9,29 +9,54 @@ Created on Wed Feb 16 19:21:18 2022
 import xml.etree.ElementTree as ET
 import os.path
 from datetime import datetime
+import re
 
-def getTestcaseStatus(testcase):
+def getTestCaseStatus(testcase):
     if testcase.find("skipped") is not None:
         status = "skipped"
     elif testcase.find("failure") is not None:
         status = "failed"
     elif testcase.find("error") is not None:
-        status = "failed"
+        status = "error"
     else:
         status = "passed"
     return status
+
+def getDependencyVersions():
+    import numpy, pandas, qtpy, PyQt5, PySide2, pyqtgraph, customQObjects, pytest, pytestqt, pytest_cov, pytest_profiling
+    imported = ["numpy", "pandas", "qtpy", "PyQt5", "pyqtgraph", 
+                "pytest", "pytest_cov", "pytest_profiling"]
+    import pkg_resources
+    dct = {}
+    for name in imported:
+        metadata = pkg_resources.get_distribution(name)
+        dct[metadata.key] = metadata.version
+    # manually do the ones pkg_resources doesn't like
+    dct['pyside2'] = PySide2.__version__ 
+    dct['customQObjects'] = customQObjects.__version__
+    dct['pytest-qt'] = pytestqt.__version__
+    
+    deps, testDeps = {}, {}
+    for key, value in sorted(dct.items()):
+        if key.startswith("pytest"):
+            testDeps[key] = value
+        else:
+            deps[key] = value
+    
+    return deps, testDeps
+
+
+def escapeHtml(html):
+    html = re.sub(r"<", "&lt;", html)
+    html = re.sub(r">", "&gt;", html)
+    return html
+    
 
 ts = None
 isoFmt = "%Y-%m-%dT%H:%M:%S.%f"
 fmt = "%d %b %Y, %H:%M:%S.%f"
 
-html = ["<html>", "<body>"]
-html += ["<style>", 
-         ".failed { background-color:#c92626; }", 
-         ".error { background-color:#c92626; }", 
-         ".skipped { background-color:#c97a26; }", 
-         ".passed { background-color:#26c954; }", 
-         "</style>"]
+html = ["<html>", "<head>", '<link rel="stylesheet" href="styles.css">' "</head>", "<body>"]
 
 resultsDir = "results"
 qtApis = ["PyQt5", "PySide2"]
@@ -50,9 +75,16 @@ else:
     ts = datetime.strptime(ts, isoFmt)
 ts = ts.strftime(fmt)
 
-html += ["<h1>Summary</h1>", f"<p>{ts}</p>"]
+html += ['<h1 id="summary">Summary</h1>', f"<p>{ts}</p>"]
 
-html += ["<table>", "<tr>"]
+html += ["<h2>Dependencies</h2>", "<table class=dependencyTable>"]
+deps, testDeps = getDependencyVersions()
+for dct in [deps, testDeps]:
+    for key, value in dct.items():
+        html += ["<tr>", f"<td>{key}</td>", f"<td>{value}</td>", "</tr>"]
+html += ["</table>"]  
+
+html += ["<h2>Test results</h2>", "<table class=summaryTable>", "<tr>"]
 tableHeader = ["Qt API", "Tests", "Passed", "Skipped", "Failed", "Errors", "Time"]
 html += [f"<th>{header}</th>" for header in tableHeader]
 for n, testsuite in enumerate(testsuites):
@@ -70,9 +102,11 @@ html += ["</table>"]
     
     
 ## test result breakdown
-html += ["<h1>Breakdown</h1>", "<table class=breakdown>"]
+html += ['<h1 id="breakdown">Breakdown</h1>', "<table class=breakdownTable>"]
 tableHeader = ["Test"] + qtApis
 html += [f"<th>{header}</th>" for header in tableHeader]
+
+notPassed = {"error":[], "failed":[], "skipped":[]}
 
 for testcase in testsuites[0].findall("testcase"):
     
@@ -80,22 +114,57 @@ for testcase in testsuites[0].findall("testcase"):
     name = testcase.attrib['name']
     time = testcase.attrib['time']
     
+    # find this test in the other testsuite
     other = testsuites[1].findall(f"*[@classname='{classname}'][@name='{name}']")[0]
     
-    status0 = getTestcaseStatus(testcase)
-    status1 = getTestcaseStatus(other)
+    status0 = getTestCaseStatus(testcase)
+    status1 = getTestCaseStatus(other)
     
+    data = ["", ""]
+    for n, status, tc in [(0, status0, testcase), (1, status1, other)]:
+        if status != "passed":
+            notPassed[status].append((qtApis[n], tc))
+            href = f"{qtApis[n]}-{classname}.{name}"
+            data[n] = f"<a href=#{href}>{tc.attrib['time']}s</a>"
+        else:
+            data[n] = f"{tc.attrib['time']}s"
+            
     html += ["<tr>", 
-             f"<td>{classname}.{name}</td>", 
-             f"<td class={status0}> {time}</td>", 
-             f"<td class={status1}> {other.attrib['time']}</td>"
+             f"<td class=testName>{classname}.{name}</td>", 
+             f"<td class={status0}>{data[0]}</td>", 
+             f"<td class={status1}>{data[1]}</td>"
              "</tr>"]
     
 html += ["</table>"]
 
+if len(notPassed["error"]) > 0:
+    html += [f'<h1 id="errorTests">Errors ({len(notPassed["error"])})</h1>']
+    # TODO
+
+if len(notPassed["failed"]) > 0:
+    html += [f'<h1 id="failedTests">Failed ({len(notPassed["failed"])})</h1>']
+    for qtApi, testcase in notPassed["failed"]:
+        testName = f"{testcase.attrib['classname']}.{testcase.attrib['name']}"
+        element = testcase.find("failure")
+        message = escapeHtml(element.attrib['message'])
+        traceback = escapeHtml(element.text)
+        html += [f'<h3 id="{qtApi}-{testName}">{testName}; {qtApi}</h3>', "<h4>Message:</h4>", 
+                 f"<span class=traceback>{message}</span>", 
+                 "<h4>Traceback:</h4>", 
+                 f"<span class=traceback>{traceback}</span>"]
+
+if len(notPassed["skipped"]) > 0:
+    html += [f'<h1 id="skippedTests">Skipped ({len(notPassed["skipped"])})</h1>']
+    for qtApi, testcase in notPassed["skipped"]:
+        testName = f"{testcase.attrib['classname']}.{testcase.attrib['name']}"
+        element = testcase.find("skipped")
+        message = escapeHtml(element.attrib['message'])
+        html += [f'<h3 id="{qtApi}-{testName}">{testName}; {qtApi}</h3>', "<h4>Message:</h4>", 
+                 f"<span class=traceback>{message}</span>"]
+
     
 html += ["<body>", "<html>"]
 html = "\n".join(html)
-with open("summary.html", "w") as fileobj:
+with open("report.html", "w") as fileobj:
     fileobj.write(html)
     
